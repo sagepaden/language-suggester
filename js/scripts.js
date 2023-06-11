051bb9477843e840dfbcdeedd7088513b10ec5e6
Sage Paden <sage.paden@gmail.com>
2023-06-11 16:03:11 -0700

    added generic descriptions of each language to the suggestions variable

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+const suggestions = {
+  ruby: {
+    name: 'Ruby',
+    description: 'A dynamic, open source programming language with a focus on simplicity and productivity.'
+  },
+  csharp: {
+    name: 'C#',
+    description: 'A modern, object-oriented programming language designed for building Windows applications.'
+  },
+  javascript: {
+    name: 'JavaScript',
+    description: 'A high-level, interpreted programming language that is widely used for creating interactive web pages.'
+  },
+  go: {
+    name: 'Go',
+    description: 'A simple, fast, and efficient programming language designed for building scalable network applications.'
+  },
+  python: {
+    name: 'Python',
+    description: 'A versatile programming language with a strong focus on simplicity and readability.'
+  },
+  rust: {
+    name: 'Rust',
+    description: 'A systems programming language that runs blazingly fast, prevents segfaults, and guarantees thread safety.'
+  },
+  swift: {
+    name: 'Swift',
+    description: 'A powerful and intuitive programming language designed for building iOS, macOS, watchOS, and tvOS apps.'
+  }
+};
